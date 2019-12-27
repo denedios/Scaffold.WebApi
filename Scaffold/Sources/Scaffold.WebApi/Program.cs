62e87f54794af82bde44997d92929599a0cc1ab1
Hannah Chan <hannah.chan@outlook.com>
2019-12-27 12:17:39 +1100

    Enable Serilog

--- Scaffold/Sources/Scaffold.WebApi/Program.cs
+++ Scaffold/Sources/Scaffold.WebApi/Program.cs
@@ -3,7 +3,9 @@
     using System.Diagnostics;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
     using Scaffold.WebApi.Extensions;
+    using Serilog;
 
     public static class Program
     {
@@ -20,9 +22,18 @@
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging => logging.ClearProviders())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
+                    webBuilder.UseSerilog(
+                        configureLogger: (hostingContext, loggerConfiguration) => loggerConfiguration
+                            .ReadFrom.Configuration(hostingContext.Configuration)
+                            .Enrich.FromLogContext()
+                            .WriteTo.Console(),
+                        preserveStaticLogger: false,
+                        writeToProviders: true);
                 });
         }
     }
